2d40aab94241b8894e840d6e7ce11cadb28e3202
Varun Athreya <varun.athreya@contentstack.com>
2023-02-02 01:18:10 +0530

    Rendered all the components in order

---
+++ 3. Javascript/4. Modular Portfolio Website/src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from "./Blog.js";
+export { default as Footer } from "./Footer.js";
+export { default as Gallery } from "./Gallery.js";
+export { default as NavBar } from "./Navbar.js";
+export { default as Profile } from "./Profile.js";
+export { default as Work } from "./Work.js";
--- 3. Javascript/4. Modular Portfolio Website/src/app.js
+++ 3. Javascript/4. Modular Portfolio Website/src/app.js
@@ -1,1 +1,24 @@
-console.log("Javascript working!");
+import {
+    Blog,
+    Footer,
+    Gallery,
+    NavBar,
+    Profile,
+    Work,
+} from "./Components/index.js";
+
+const root = document.getElementById("root");
+
+const blog = new Blog();
+const footer = new Footer();
+const gallery = new Gallery();
+const nav = new NavBar();
+const profile = new Profile();
+const work = new Work();
+
+nav.mount(root);
+profile.mount(root);
+work.mount(root);
+blog.mount(root);
+gallery.mount(root);
+footer.mount(root);
