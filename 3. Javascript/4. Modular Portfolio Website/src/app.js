20e054b7120b953a8116286cff1570708715b8f8
Varun Athreya <varun.athreya@contentstack.com>
2023-02-02 02:00:25 +0530

    Improved code readability on app.js

--- 3. Javascript/4. Modular Portfolio Website/src/app.js
+++ 3. Javascript/4. Modular Portfolio Website/src/app.js
@@ -9,16 +9,11 @@ import {
 
 const root = document.getElementById("root");
 
-const blog = new Blog();
-const footer = new Footer();
-const gallery = new Gallery();
-const nav = new NavBar();
-const profile = new Profile();
-const work = new Work();
-
-nav.mount(root);
-profile.mount(root);
-work.mount(root);
-blog.mount(root);
-gallery.mount(root);
-footer.mount(root);
+[
+    new NavBar(),
+    new Profile(),
+    new Work(),
+    new Blog(),
+    new Gallery(),
+    new Footer(),
+].forEach((comp) => comp.mount(root));
