f33fc7ff9275aca3fd42ba7c36cadcd32509ff5d
Varun Athreya <varun.athreya@contentstack.com>
2023-02-01 20:58:38 +0530

    Created json file from the html data

---
+++ 3. Javascript/4. Modular Portfolio Website/src/data/index.js
@@ -0,0 +1,237 @@
+export const navTitle = "VSA";
+
+export const profileLinks = [
+    {
+        url: "https://github.com/VarunSAthreya",
+        alt: "github link",
+        img: "./assets/github.svg",
+    },
+    {
+        url: "https://linkedin.com/in/varunsathreya",
+        alt: "linkedin link",
+        img: "./assets/linkedin.svg",
+    },
+];
+
+export const personalDetails = {
+    image: {
+        alt: "profile image",
+        src: "./assets/varun.png",
+    },
+    heading: "Hi, my name is Varun",
+    description:
+        "I'm a Mobile 📱 and Web 🌐 developer! Currently pursuing Bachelor of Engineering in Computer Science and working as an Associate Software Engineering Intern at Contentstack. Besides programming, I enjoy taking Photos and watching Anime.",
+};
+
+export const profileButtons = [
+    {
+        classname: "white",
+        href: "#work",
+        text: "Work",
+    },
+    {
+        classname: "white",
+        href: "#blog",
+        text: "Blog",
+    },
+    {
+        classname: "white",
+        href: "#gallery",
+        text: "Gallery",
+    },
+];
+
+export const workDetails = {
+    title: "Work",
+    description:
+        "Below are the technologies which I have worked extensively withand few projects which uses few of these technologies.",
+    chips: [
+        {
+            color: "#61dbfb",
+            text: "react",
+        },
+        {
+            color: "#333333",
+            text: "nextjs",
+        },
+        {
+            color: "#f5820d",
+            text: "firebase",
+        },
+        {
+            color: "#5b5a5a",
+            text: "express",
+        },
+        {
+            color: "#22f110",
+            text: "node",
+        },
+        {
+            color: "#3fa037",
+            text: "mongo db",
+        },
+        {
+            color: "#053f66",
+            text: "postgresql",
+        },
+        {
+            color: "#f0db4f",
+            text: "javascript",
+        },
+        {
+            color: "#007acc",
+            text: "typescript",
+        },
+        {
+            color: "#e34c26",
+            text: "html",
+        },
+        {
+            color: "#e656ccf9",
+            text: "graphql",
+        },
+        {
+            color: "#2965f1",
+            text: "css",
+        },
+        {
+            color: "#257bdc",
+            text: "flutter",
+        },
+    ],
+};
+
+export const projectData = [
+    {
+        image: {
+            src: "./assets/placement-management.png",
+            alt: "project 1",
+        },
+        title: "Placement Management",
+        description:
+            "The Placement Management portal is a tool that helps students and administrators coordinate college placement.",
+        href: "https://github.com/VarunSAthreya/placement-management",
+    },
+    {
+        image: {
+            src: "./assets/rise-against-hunger.png",
+            alt: "project 2",
+        },
+        title: "Rise against hunger",
+        description:
+            "Rise Against hunger is an web and mobile app that helps to combat hunger. It provides a forum to donate food and to take surveys for hunger hot-spots. The web portal also has an admin panel to look at each and every detail about the forum.",
+        href: "https://github.com/VarunSAthreya/rise-against-hunger",
+    },
+    {
+        image: {
+            src: "./assets/mental-health-matters.png",
+            alt: "project 3",
+        },
+        title: "Mental Health Matters",
+        description:
+            "Mental Health Matters is a web app that is used to schedule appointments and take surveys which is used to for resolving mental health-related issues.",
+        href: "https://github.com/VarunSAthreya/mental-health-matters",
+    },
+];
+
+export const blogHeadingDetails = {
+    title: "Blogs",
+    description: "Below are the few of the blogs I have written.",
+};
+
+export const blogData = [
+    {
+        image: {
+            src: "https://varunsathreya.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Funsplash%2FGll-v69L8iA%2Fupload%2Fv1661977373533%2FMcCCWN_bu.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
+            alt: "blog 1",
+        },
+        title: "Flutter Basic to Advance Concepts!",
+        description:
+            "Learning basic to advance concepts in Flutter using taking Note App.",
+        href: "https://varunsathreya.hashnode.dev/flutter-basic-to-advance-concepts",
+    },
+    {
+        image: {
+            src: "https://varunsathreya.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1671730472796%2F474a3323-5651-47c4-941e-f51e4bc5d04c.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
+            alt: "blog 2",
+        },
+        title: "Implementing GraphQL using Typescript and Node JS",
+        description:
+            "GraphQL is a query language for your API that allows clients to request exactly the data they need, and nothing more. It is often used as an alternative to REST APIs, which require clients to request fixed sets of data and may return unnecessary data.",
+        href: "https://varunsathreya.hashnode.dev/implementing-graphql-using-typescript-and-node-js",
+    },
+    {
+        image: {
+            src: "https://varunsathreya.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1671686627468%2FNXwHmCTqp.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
+            alt: "blog 3",
+        },
+        title: "Deep Dive to TailwindCSS using ReactJS",
+        description:
+            "Tailwind CSS is a utility-first CSS framework that allows you to build custom designs by composing CSS classes.",
+        href: "https://varunsathreya.hashnode.dev/deep-dive-to-tailwindcss-using-reactjs",
+    },
+];
+
+export const footerData = "&copy;2023 - <code> VarunSAthreya </code>";
+
+export const galleryDetails = {
+    title: "Gallery",
+    description: "Below are the few of the photos I have taken.",
+};
+
+export const photoDetails = [
+    {
+        src: "./assets/photos/1.jpeg",
+        href: "#img_1",
+        id: "img_1",
+        lightbox_href: "#_1",
+    },
+    {
+        src: "./assets/photos/2.jpeg",
+        href: "#img_2",
+        id: "img_2",
+        lightbox_href: "#_2",
+    },
+    {
+        src: "./assets/photos/3.jpeg",
+        href: "#img_3",
+        id: "img_3",
+        lightbox_href: "#_3",
+    },
+    {
+        src: "./assets/photos/4.jpeg",
+        href: "#img_4",
+        id: "img_4",
+        lightbox_href: "#_4",
+    },
+    {
+        src: "./assets/photos/5.jpeg",
+        href: "#img_5",
+        id: "img_5",
+        lightbox_href: "#_5",
+    },
+    {
+        src: "./assets/photos/6.jpeg",
+        href: "#img_6",
+        id: "img_6",
+        lightbox_href: "#_6",
+    },
+    {
+        src: "./assets/photos/7.jpeg",
+        href: "#img_7",
+        id: "img_7",
+        lightbox_href: "#_7",
+    },
+    {
+        src: "./assets/photos/8.jpeg",
+        href: "#img_8",
+        id: "img_8",
+        lightbox_href: "#_8",
+    },
+    {
+        src: "./assets/photos/9.jpeg",
+        href: "#img_9",
+        id: "img_9",
+        lightbox_href: "#_9",
+    },
+];
