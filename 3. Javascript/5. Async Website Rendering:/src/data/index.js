81b9e79a8b79cab898fd6a4ee0d640ff581d2acb
Varun Athreya <varun.athreya@contentstack.com>
2023-02-03 01:18:01 +0530

    Added static data and url

---
+++ 3. Javascript/5. Async Website Rendering:/src/data/index.js
@@ -0,0 +1,28 @@
+export const navTitle = "VSA";
+
+export const blogHeadingDetails = {
+    title: "Blogs",
+    description: "Below are the few of the blogs I have written.",
+};
+
+export const footerData = "&copy;2023 - <code> VarunSAthreya </code>";
+
+export const galleryDetails = {
+    title: "Gallery",
+    description: "Below are the few of the photos I have taken.",
+};
+
+const blog_url =
+    "https://raw.githubusercontent.com/VarunSAthreya/contentstack-training/main/3.%20Javascript/5.%20Async%20Website%20Rendering%3A/data/blog.json";
+const gallery_url =
+    "https://raw.githubusercontent.com/VarunSAthreya/contentstack-training/main/3.%20Javascript/5.%20Async%20Website%20Rendering%3A/data/gallery.json";
+const personal_details_url =
+    "https://raw.githubusercontent.com/VarunSAthreya/contentstack-training/main/3.%20Javascript/5.%20Async%20Website%20Rendering%3A/data/personal-data.json";
+const profile_button =
+    "https://raw.githubusercontent.com/VarunSAthreya/contentstack-training/main/3.%20Javascript/5.%20Async%20Website%20Rendering%3A/data/profile-buttons.json";
+const profile_links =
+    "https://raw.githubusercontent.com/VarunSAthreya/contentstack-training/main/3.%20Javascript/5.%20Async%20Website%20Rendering%3A/data/profile-links.json";
+const project_linlk =
+    "https://raw.githubusercontent.com/VarunSAthreya/contentstack-training/main/3.%20Javascript/5.%20Async%20Website%20Rendering%3A/data/projects.json";
+const work_link =
+    "https://raw.githubusercontent.com/VarunSAthreya/contentstack-training/main/3.%20Javascript/5.%20Async%20Website%20Rendering%3A/data/work.json";
